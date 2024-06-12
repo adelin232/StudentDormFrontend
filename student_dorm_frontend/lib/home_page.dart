4c6583a0dd39175bbd927793cc81a7b80783b86a
adelin232 <narcis.adelin.miulet@gmail.com>
2024-06-12 22:43:22 +0300

    Final

--- student_dorm_frontend/lib/home_page.dart
+++ student_dorm_frontend/lib/home_page.dart
@@ -100,8 +100,8 @@ class _HomePageState extends State<HomePage> {
                         text: 'Pagina de administrare',
                         onTap: () => _navigateTo('/admin'),
                       ),
-                    if (_isAdmin) const SizedBox(height: 20),
-                    if (_isAdmin)
+                    if (!_isAdmin) const SizedBox(height: 20),
+                    if (!_isAdmin)
                       buildCard(
                         icon: Icons.announcement,
                         text: 'Feed anunțuri',
@@ -113,22 +113,22 @@ class _HomePageState extends State<HomePage> {
                       text: 'Modifică profilul',
                       onTap: () => _navigateTo('/profile'),
                     ),
-                    if (_isAdmin) const SizedBox(height: 20),
-                    if (_isAdmin)
+                    if (!_isAdmin) const SizedBox(height: 20),
+                    if (!_isAdmin)
                       buildCard(
                         icon: Icons.local_laundry_service,
                         text: 'Rezervă o mașină de spălat',
                         onTap: () => _navigateTo('/booking'),
                       ),
-                    if (_isAdmin) const SizedBox(height: 20),
-                    if (_isAdmin)
+                    if (!_isAdmin) const SizedBox(height: 20),
+                    if (!_isAdmin)
                       buildCard(
                         icon: Icons.list,
                         text: 'Rezervările mele',
                         onTap: () => _navigateTo('/mybookings'),
                       ),
-                    if (_isAdmin) const SizedBox(height: 20),
-                    if (_isAdmin)
+                    if (!_isAdmin) const SizedBox(height: 20),
+                    if (!_isAdmin)
                       buildCard(
                         icon: Icons.report_problem,
                         text: 'Realizează o plângere',
